b00d0d10554f9ee789e4f60a417e833fb5298ece
Steven Le <stevenhle459@gmail.com>
2021-11-07 12:11:02 -0600

    Porting to GDScript

--- scripts/main_menu.tsx
+++
@@ -1,9 +0,0 @@
-import { signal, property, tool, onready, node } from "../decorators";
-
-@tool
-export default class MainMenu extends godot.Control {
-    
-    _on_button_pressed() {
-        this.get_tree().change_scene("res://scenes/opening.tscn");
-    }
-}
